d6f75fbd144bbe03a80bad6937bd2420e015e1dc
marcelobarth <marcelinhobarth@hotmail.com>
2020-08-01 12:47:34 -0300

    Antes da Home

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CadastrarVideo from "./pages/cadastro/Video";
+
+const Pagina404 = () => <div>Página 404</div>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastrarVideo} />
+      <Route path="/" component={App} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CadastrarVideo() {
+  return <div>Cadastro Video</div>;
+}
+
+export default CadastrarVideo;
